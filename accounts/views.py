7245846708210054920c7a94c7b5f6415e7618dd
ziad <zeyad.officemail@gmail.com>
2026-01-29 20:42:01 +0300

    feat: add JWT login, refresh, and profile endpoints

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
 from rest_framework import generics, permissions
 from .serializers import RegisterSerializer
 from .models import User
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
+
+class ProfileView(APIView):
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email
+        })
