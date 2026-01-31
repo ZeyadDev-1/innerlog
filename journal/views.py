3ca158e48f394682bda19c203a6960b63df3402a
ziad <zeyad.officemail@gmail.com>
2026-02-01 00:19:30 +0300

    feat: add list, create, and delete APIs for mood entries

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import MoodEntry
+from .serializers import MoodEntrySerializer
 
-# Create your views here.
+class MoodEntryListCreateView(generics.ListCreateAPIView):
+    serializer_class = MoodEntrySerializer
+
+    def get_queryset(self):
+        return MoodEntry.objects.filter(user=self.request.user)
+
+
+class MoodEntryDeleteView(generics.DestroyAPIView):
+    serializer_class = MoodEntrySerializer
+
+    def get_queryset(self):
+        return MoodEntry.objects.filter(user=self.request.user)
