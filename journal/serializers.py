5b5f4cc4aa76bc9c60f61b0a5c44f607e52b072c
ziad <zeyad.officemail@gmail.com>
2026-02-01 00:18:14 +0300

    feat: add serializer with ownership enforcement for mood entries

---
+++ journal/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import MoodEntry
+
+
+class MoodEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MoodEntry
+        fields = [
+            'id',
+            'mood_score',
+            'journal_text',
+            'emotions',
+            'created_at',
+        ]
+        read_only_fields = ['id', 'created_at']
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        validated_data['user'] = request.user
+        return super().create(validated_data)
