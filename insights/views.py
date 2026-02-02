58422e7d269307fa5a76d29f7e791d1e2885a5f6
ziad <zeyad.officemail@gmail.com>
2026-02-02 18:48:25 +0300

    feat: add mood trend analytics endpoint

--- insights/views.py
+++ insights/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from journal.models import MoodEntry
 
-# Create your views here.
+class MoodTrendView(APIView):
+    def get(self, request):
+        entries = MoodEntry.objects.filter(user=request.user).order_by('created_at')
+
+        data = [
+            {
+                "date": entry.created_at.date(),
+                "mood_score": entry.mood_score
+            }
+            for entry in entries
+        ]
+
+        return Response(data)
+
