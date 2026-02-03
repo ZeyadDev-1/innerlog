28301e9a783fb4b016d16aa950a025ca6ef5614a
ziad <zeyad.officemail@gmail.com>
2026-02-03 22:49:31 +0300

    feat: add Resource model for well-being recommendations

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Resource(models.Model):
+    RESOURCE_TYPES = [
+        ('article', 'Article'),
+        ('exercise', 'Exercise'),
+        ('prompt', 'Journal Prompt'),
+        ('external', 'External Link'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    resource_type = models.CharField(
+        max_length=20,
+        choices=RESOURCE_TYPES
+    )
+    url = models.URLField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
