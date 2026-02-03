d93c2e5884b9662c1d47e0c5bb9d3e645d400d4a
ziad <zeyad.officemail@gmail.com>
2026-02-03 23:31:36 +0300

    feat: add rule-based resource recommendation engine

--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Avg
+from journal.models import MoodEntry
+from .models import Resource
+from .serializers import ResourceSerializer
 
-# Create your views here.
+class ResourceRecommendationView(APIView):
+    def get(self, request):
+        user = request.user
+
+        avg_mood = (
+            MoodEntry.objects
+            .filter(user=user)
+            .aggregate(avg=Avg('mood_score'))
+            .get('avg')
+        )
+
+        queryset = Resource.objects.filter(is_active=True)
+
+        if avg_mood is not None and avg_mood <= 2.5:
+            queryset = queryset.filter(resource_type='exercise')
+        elif avg_mood is not None and avg_mood >= 4:
+            queryset = queryset.filter(resource_type='article')
+
+        serializer = ResourceSerializer(queryset, many=True)
+        return Response({
+            "average_mood": avg_mood,
+            "resources": serializer.data,
+            "disclaimer": (
+                "These resources are for general well-being support "
+                "and are not medical advice."
+            )
+        })
