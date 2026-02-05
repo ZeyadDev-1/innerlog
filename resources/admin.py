2ba85bc6758ad9d65173d36245255c160473b7ad
ziad <zeyad.officemail@gmail.com>
2026-02-05 23:34:57 +0300

    feat: register Resource model with admin customization

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Resource
 
-# Register your models here.
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'resource_type',
+        'is_active',
+    )
+    list_filter = (
+        'resource_type',
+        'is_active',
+    )
+    search_fields = (
+        'title',
+        'description',
+    )
+
