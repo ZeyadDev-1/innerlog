5ef172f22952c3dd0fa69c0d445171cf6e8bac50
ziad <zeyad.officemail@gmail.com>
2026-02-05 23:45:40 +0300

    refactor: improve admin usability for resource management

--- resources/admin.py
+++ resources/admin.py
@@ -4,17 +4,9 @@ from .models import Resource
 
 @admin.register(Resource)
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = (
-        'title',
-        'resource_type',
-        'is_active',
-    )
-    list_filter = (
-        'resource_type',
-        'is_active',
-    )
-    search_fields = (
-        'title',
-        'description',
-    )
+    list_display = ('title', 'resource_type', 'is_active')
+    list_filter = ('resource_type', 'is_active')
+    search_fields = ('title', 'description')
+    ordering = ('resource_type', 'title')
+
 
