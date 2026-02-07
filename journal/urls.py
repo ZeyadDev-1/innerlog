2ebf948b611c6ae123439432d8b71b5341044cd1
ziad <zeyad.officemail@gmail.com>
2026-02-07 23:30:45 +0300

    feat: add user data export endpoint for mood journal

--- journal/urls.py
+++ journal/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MoodEntryListCreateView, MoodEntryDeleteView
+from .views import MoodEntryListCreateView, MoodEntryDeleteView, ExportMoodDataView
 
 urlpatterns = [
     path('moods/', MoodEntryListCreateView.as_view()),
     path('moods/<int:pk>/', MoodEntryDeleteView.as_view()),
+    path('export/', ExportMoodDataView.as_view()),
 ]
--- journal/views.py
+++ journal/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import MoodEntry
 from .serializers import MoodEntrySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class MoodEntryListCreateView(generics.ListCreateAPIView):
     serializer_class = MoodEntrySerializer
@@ -15,3 +17,22 @@ class MoodEntryDeleteView(generics.DestroyAPIView):
 
     def get_queryset(self):
         return MoodEntry.objects.filter(user=self.request.user)
+
+class ExportMoodDataView(APIView):
+    def get(self, request):
+        entries = MoodEntry.objects.filter(user=request.user)
+
+        data = [
+            {
+                "date": entry.created_at,
+                "mood_score": entry.mood_score,
+                "emotions": entry.emotions,
+                "journal_text": entry.journal_text,
+            }
+            for entry in entries
+        ]
+
+        return Response({
+            "user": request.user.username,
+            "entries": data
+        })
