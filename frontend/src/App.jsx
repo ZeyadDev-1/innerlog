9580cf4f074f735659a344a5f2c2e933d34f52dc
ziad <zeyad.officemail@gmail.com>
2026-02-10 16:45:26 +0300

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import api from "./api/client";
 import Login from "./pages/Login";
 import MoodForm from "./components/MoodForm";
 import MoodTrendChart from "./charts/MoodTrendChart";
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState(
-    !!localStorage.getItem("access_token")
-  );
+  const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("access_token"));
   const [trend, setTrend] = useState([]);
 
-  async function loadTrend() {
+  const loadData = async () => {
     const res = await api.get("insights/trend/");
     setTrend(res.data);
-  }
+  };
 
   useEffect(() => {
-    if (loggedIn) {
-      loadTrend();
-    }
+    if (loggedIn) loadData();
   }, [loggedIn]);
 
-  if (!loggedIn) {
-    return <Login onLogin={() => setLoggedIn(true)} />;
-  }
+  if (!loggedIn) return <Login onLogin={() => setLoggedIn(true)} />;
 
   return (
-    <div>
-      <MoodForm onAdd={loadTrend} />
+    <>
+      <MoodForm onAdd={loadData} />
       <MoodTrendChart data={trend} />
-    </div>
+    </>
   );
 }
