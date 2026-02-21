895968bee16bc332688df6d80e94fc0b3507c11f
ziad <zeyad.officemail@gmail.com>
2026-02-21 23:18:42 +0300

    update serializers

--- journal/serializers.py
+++ journal/serializers.py
@@ -6,15 +6,16 @@ class MoodEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = MoodEntry
         fields = [
-            'id',
-            'mood_score',
-            'journal_text',
-            'emotions',
-            'created_at',
+            "id",
+            "mood_score",
+            "journal_text",
+            "emotions",
+            "created_at",
         ]
-        read_only_fields = ['id', 'created_at']
+        read_only_fields = ["id", "created_at"]
 
     def create(self, validated_data):
-        request = self.context.get('request')
-        validated_data['user'] = request.user
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            validated_data["user"] = request.user
         return super().create(validated_data)
