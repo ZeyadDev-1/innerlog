e5f6d49cd6ef8963811d7908dfd6e147757b7e0c
ziad <zeyad.officemail@gmail.com>
2026-02-27 16:45:04 +0300

    feat: register creates inactive user and emails verification link

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import get_user_model
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -14,4 +15,23 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             password=validated_data['password']
         )
+        return user
+    
+User = get_user_model()
+
+class RegisterWithEmailSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+
+        # IMPORTANT: user inactive until email verified
+        user.is_active = False
+        user.save()
         return user
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,14 @@ from .models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+
+from .serializers import RegisterWithEmailSerializer
+from .tokens import email_verification_token
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -25,4 +33,26 @@ class DeleteAccountView(APIView):
         return Response(
             {"detail": "Account and all associated data deleted."},
             status=status.HTTP_204_NO_CONTENT
+        )
+    
+User = get_user_model()
+
+class RegisterWithEmailView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterWithEmailSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = email_verification_token.make_token(user)
+
+        verify_url = f"{settings.FRONTEND_URL}/verify-email?uid={uid}&token={token}"
+
+        send_mail(
+            subject="Verify your InnerLog account",
+            message=f"Click to verify your email: {verify_url}",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
         )
